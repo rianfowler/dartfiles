3deb3d3caf0a037e4ed66b3691ac0af6424463bf
Rian Fowler <4998130+rianfowler@users.noreply.github.com>
2022-01-06 02:01:04 +0000

    add remove command to omz command

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ ohmyzsh() {
     echo "==========================================================="
     echo "                  Install oh-my-zsh                        "
     echo "-----------------------------------------------------------"
+    rm -rf /home/codespace/.oh-my-zsh
     sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 }
 
